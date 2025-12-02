ef2ed9610609a3d400e31b50b09b8688cedea9f3
Majid Taheri <taheri.interview@gmail.com>
2025-12-02 20:26:12 +0330

    [R&D] introduce adapter.tsx

---
+++ frontend/src/x/adapter.tsx
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import type { XFeature } from "../types/xfeature";
+
+class XAdapter{
+    model:XFeature;
+    constructor( model:XFeature){
+        this.model=model;
+        this.wrapper=this.wrapper.bind(this);
+    }
+
+    wrapper(p:wrapperProps):React.ReactNode{ 
+        return <context.Provider value={this}>
+            {p.children}
+        </context.Provider>
+    }
+    datatable(p:dataTableProps){
+        const {model}=useContext(context);// replacement of this  
+        const datatable=model.frontend.dataTables.find(t=>t.id==p.id)
+        // TODO: Hey AI, USE : https://mui.com/material-ui/react-table/
+        /* TODO: follows this code 
+        <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        sx={{ border: 0 }}
+      />
+
+        */ 
+        return <></> 
+    }
+}
+interface wrapperProps{
+    children:React.ReactNode;
+}
+interface dataTableProps{
+    id:string;
+
+}
+const context=React.createContext<XAdapter>(null as unknown as XAdapter)
+ 
