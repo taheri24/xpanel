89bcf3bec8d9eae2814faa60a6df7ad49d09bfb5
Majid Taheri <taheri.interview@gmail.com>
2025-12-02 21:57:04 +0330

    add TODOs

--- frontend/src/x/adapter.tsx
+++ frontend/src/x/adapter.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import type { XFeature } from "../types/xfeature";
-
+import {GridColDef} from '@mui/x-data-grid'
 class XAdapter{
     model:XFeature;
     constructor( model:XFeature){
@@ -14,8 +14,12 @@ class XAdapter{
         </context.Provider>
     }
     datatable(p:dataTableProps){
-        const {model}=useContext(context);// replacement of this  
-        const datatable=model.frontend.dataTables.find(t=>t.id==p.id)
+        const adap=useContext(context);// replacement of this  
+        const dt=adap.model.frontend.dataTables.find(t=>t.id==p.id);
+         
+        const columns:Array<GridColDef>=[]; // TODO : convert dt?.columns to GridColDef[] with using `map`
+
+          
         // TODO: Hey AI, USE : https://mui.com/material-ui/react-table/
         /* TODO: follows this code 
         <DataGrid
